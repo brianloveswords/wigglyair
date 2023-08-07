2f689f6cbe6176ea782bbcbfbcaab8e2fed7e8bd
Brian J Brennan <brianloveswords@gmail.com>
2023-08-06 22:11:36 -0400

    remove some trash

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,5 @@
 use crate::configuration::Settings;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use std::sync::Arc;
 
 #[derive(Debug)]
@@ -8,19 +8,6 @@ pub struct AppState {
 }
 
 pub type SharedState = Arc<AppState>;
-
-// the input to our `create_user` handler
-#[derive(Debug, Deserialize)]
-pub struct CreateUser {
-    pub username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Debug, Serialize)]
-pub struct User {
-    pub id: u64,
-    pub username: String,
-}
 
 #[derive(Debug, Serialize)]
 pub struct DebugResponse {
