1432912ffcec991acf7cb7e347772cf9701e2b71
Brian J Brennan <brianloveswords@gmail.com>
2023-08-10 21:30:29 -0400

    add AsyncDatabase
    trying to avoid all blocking stuff to see how much it impacts performance

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,6 @@
 use rusqlite::Connection;
+use tokio_rusqlite::Connection as AsyncConnection;
+
 pub type Migrations<'a> = rusqlite_migration::Migrations<'a>;
 pub type M<'a> = rusqlite_migration::M<'a>;
 
@@ -24,7 +26,41 @@ impl Database {
         conn.pragma_update(None, "journal_mode", &"WAL")
             .expect("Failed to set journal mode");
 
-        apply_migrations(&mut conn, migrations);
+        migrations
+            .to_latest(&mut conn)
+            .expect("Failed to apply migrations");
+
+        Self { conn }
+    }
+}
+
+pub struct AsyncDatabase {
+    pub conn: AsyncConnection,
+}
+
+impl AsyncDatabase {
+    pub async fn connect<'a>(kind: DatabaseKind) -> Self {
+        let conn = match kind {
+            DatabaseKind::File(path) => {
+                tracing::info!("Opening database at {}", path);
+                AsyncConnection::open(path)
+                    .await
+                    .expect("Failed to open connection")
+            }
+            DatabaseKind::Memory => {
+                tracing::info!("Opening in-memory database");
+                AsyncConnection::open_in_memory()
+                    .await
+                    .expect("Failed to open connection")
+            }
+        };
+
+        conn.call(move |conn| {
+            // see: https://cj.rs/blog/sqlite-pragma-cheatsheet-for-performance-and-consistency/
+            conn.pragma_update(None, "journal_mode", &"WAL")
+        })
+        .await
+        .expect("Failed to set journal mode");
 
         Self { conn }
     }
@@ -55,11 +91,4 @@ impl DatabaseKind {
             Self::File(path.into())
         }
     }
-}
-
-#[tracing::instrument(skip(conn))]
-fn apply_migrations(conn: &mut Connection, migrations: Migrations) {
-    migrations
-        .to_latest(conn)
-        .expect("Failed to apply migrations");
 }
