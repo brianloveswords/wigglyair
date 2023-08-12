7819ecc978168cdab95761254c6d44573aa6b1f4
Brian J Brennan <brianloveswords@gmail.com>
2023-08-12 15:25:10 -0400

    play a sine wave
    this plays without skips/pops!
    
    I tested by doing `cross build --release` while playing the sine wave and even with my CPUs saturated the sine wave played seamlessly.
    
    now I need to figure out how to turn flac files into the raw per-channel samples.

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -1,4 +1,8 @@
+use audio_thread_priority::{
+    demote_current_thread_from_real_time, promote_current_thread_to_real_time,
+};
 use clap::Parser;
+use tinyaudio::prelude::*;
 use tracing_unwrap::ResultExt;
 use wigglyair::configuration;
 
@@ -11,11 +15,37 @@ struct Cli {
 
 #[tokio::main]
 async fn main() {
+    // when this leaves scope the logs will be flushed
     let _guard = configuration::setup_tracing_async("play".into());
 
     let cli = Cli::parse();
     let audio_file = cli.audio_file;
 
     tracing::info!(audio_file, "Playing {audio_file}");
-    Err("trying this out").unwrap_or_log()
+
+    let params = OutputDeviceParameters {
+        channels_count: 2,
+        sample_rate: 44100,
+        channel_sample_count: 4410,
+    };
+
+    // play a sine wave
+    let _device = run_output_device(params, {
+        let mut clock = 0f32;
+        move |data| {
+            tracing::info!("Writing {} samples", data.len());
+            promote_current_thread_to_real_time(1024, 44100).unwrap_or_log();
+            for samples in data.chunks_mut(params.channels_count) {
+                clock = (clock + 1.0) % params.sample_rate as f32;
+                let value =
+                    (clock * 440.0 * 2.0 * std::f32::consts::PI / params.sample_rate as f32).sin();
+                for sample in samples {
+                    *sample = value;
+                }
+            }
+        }
+    })
+    .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(30));
 }
