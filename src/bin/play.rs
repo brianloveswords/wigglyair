04fede30675f6210f1c2383b79829f994c6e6188
Brian J Brennan <brianloveswords@gmail.com>
2023-08-12 20:14:28 -0400

    ugly as hell but gapless playback of multiple files works
    works well, but I think it's using 100% CPU to do it. might be good to go sending audio buffers instead of individual samples.

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -20,7 +20,7 @@ struct Cli {
     file2: String,
 }
 
-const SAMPLE_RATE: usize = 44100;
+const DEFAULT_SAMPLE_RATE: usize = 44100;
 const CHANNEL_COUNT: usize = 2;
 
 fn main() {
@@ -29,90 +29,98 @@ fn main() {
 
     let cli = Cli::parse();
     let file1 = cli.file1;
-    let _file2 = cli.file2;
+    let file2 = cli.file2;
 
     // create channel for individual samples (interleaved format)
-    let (sample_tx, sample_rx) = bounded::<f32>(SAMPLE_RATE * CHANNEL_COUNT * 30);
+    let (sample_tx, sample_rx) = bounded::<f32>(DEFAULT_SAMPLE_RATE * CHANNEL_COUNT * 30);
 
     // create channel for the sample rate of the first track
     let (rate_tx, rate_rx) = bounded::<usize>(1);
-    let mut rate_tx = Some(rate_tx);
 
-    let probed = {
-        let file = Box::new(File::open(Path::new(&file1)).unwrap_or_log());
-        symphonia::default::get_probe()
-            .format(
-                &Hint::new(),
-                MediaSourceStream::new(file, Default::default()),
-                &Default::default(),
-                &Default::default(),
-            )
-            .unwrap()
-    };
-
-    let mut format = probed.format;
-    let track = format.default_track().unwrap();
-
-    let mut decoder = symphonia::default::get_codecs()
-        .make(&track.codec_params, &Default::default())
-        .unwrap();
-
-    // Store the track identifier, we'll use it to filter packets.
-    let track_id = track.id;
-
-    let mut sample_buf = None;
-
+    let files = vec![file1, file2];
     thread::spawn(move || {
-        loop {
-            let packet = match format.next_packet() {
-                Ok(packet) => packet,
-                Err(Error::IoError(e)) if e.kind() == io::ErrorKind::UnexpectedEof => break,
-                Err(err) => {
-                    tracing::error!(?err, "Error reading packet");
-                    panic!("Error reading packet")
-                }
+        let mut rate_sent = false;
+        for file in files {
+            tracing::info!("Reading file: {}", file);
+
+            let probed = {
+                let file = Box::new(File::open(Path::new(&file)).unwrap_or_log());
+                symphonia::default::get_probe()
+                    .format(
+                        &Hint::new(),
+                        MediaSourceStream::new(file, Default::default()),
+                        &Default::default(),
+                        &Default::default(),
+                    )
+                    .unwrap()
             };
 
-            // If the packet does not belong to the selected track, skip it.
-            if packet.track_id() != track_id {
-                continue;
-            }
+            let mut format = probed.format;
+            let track = format.default_track().unwrap();
 
-            match decoder.decode(&packet) {
-                Ok(audio_buf) => {
-                    if sample_buf.is_none() {
-                        let spec = *audio_buf.spec();
-                        tracing::info!(?spec, "Decoded audio buffer spec");
+            let mut decoder = symphonia::default::get_codecs()
+                .make(&track.codec_params, &Default::default())
+                .unwrap();
 
-                        if let Some(rate_tx) = rate_tx.take() {
-                            rate_tx.send(spec.rate as usize).unwrap_or_log();
-                        }
+            // Store the track identifier, we'll use it to filter packets.
+            let track_id = track.id;
 
-                        let duration = audio_buf.capacity() as u64;
-                        tracing::info!(?duration, "Decoded audio buffer duration");
+            let mut sample_buf = None;
 
-                        sample_buf = Some(SampleBuffer::<f32>::new(duration, spec));
+            loop {
+                let packet = match format.next_packet() {
+                    Ok(packet) => packet,
+                    Err(Error::IoError(e)) if e.kind() == io::ErrorKind::UnexpectedEof => break,
+                    Err(err) => {
+                        tracing::error!(?err, "Error reading packet");
+                        panic!("Error reading packet")
                     }
+                };
 
-                    // Copy the decoded audio buffer into the sample buffer in an interleaved format.
-                    if let Some(buf) = &mut sample_buf {
-                        buf.copy_interleaved_ref(audio_buf);
-                        for sample in buf.samples() {
-                            sample_tx.send(*sample).unwrap();
-                        }
-                    }
+                // If the packet does not belong to the selected track, skip it.
+                if packet.track_id() != track_id {
+                    continue;
                 }
-                Err(Error::DecodeError(err)) => tracing::error!(?err, "audio loop: decode error"),
-                Err(err) => {
-                    tracing::error!(?err, "audio loop: error");
-                    break;
+
+                match decoder.decode(&packet) {
+                    Ok(audio_buf) => {
+                        if sample_buf.is_none() {
+                            let spec = *audio_buf.spec();
+                            tracing::info!(?spec, "Decoded audio buffer spec");
+
+                            if !rate_sent {
+                                rate_tx.send(spec.rate as usize).unwrap_or_log();
+                                rate_sent = true;
+                            }
+
+                            let duration = audio_buf.capacity() as u64;
+                            tracing::info!(?duration, "Decoded audio buffer duration");
+
+                            sample_buf = Some(SampleBuffer::new(duration, spec));
+                        }
+
+                        // Copy the decoded audio buffer into the sample buffer in an interleaved format.
+                        if let Some(buf) = &mut sample_buf {
+                            buf.copy_interleaved_ref(audio_buf);
+                            for sample in buf.samples() {
+                                sample_tx.send(*sample).unwrap();
+                            }
+                        }
+                    }
+                    Err(Error::DecodeError(err)) => {
+                        tracing::error!(?err, "audio loop: decode error")
+                    }
+                    Err(err) => {
+                        tracing::error!(?err, "audio loop: error");
+                        break;
+                    }
                 }
             }
         }
     });
 
     let sample_rate = rate_rx.recv().unwrap_or_log();
-    tracing::info!(file1, sample_rate, "Playing {file1} at {sample_rate} Hz");
+    tracing::info!(sample_rate, "Playing at {sample_rate} Hz");
 
     let params = OutputDeviceParameters {
         channels_count: CHANNEL_COUNT,
@@ -124,9 +132,9 @@ fn main() {
         let mut promoted = false;
         move |data| {
             if !promoted {
-                let tid =
-                    promote_current_thread_to_real_time(0, SAMPLE_RATE as u32).unwrap_or_log();
-                tracing::info!(?tid, "thread promoted");
+                let tid = promote_current_thread_to_real_time(0, DEFAULT_SAMPLE_RATE as u32)
+                    .unwrap_or_log();
+                tracing::info!(?tid, "Thread promoted");
                 promoted = true;
             }
             for sample in data.iter_mut() {
