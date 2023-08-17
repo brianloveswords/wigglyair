54a309fa21d299d2d31a4be779b9534ea65ceb7a
Brian J Brennan <brianloveswords@gmail.com>
2023-08-16 20:31:01 -0400

    testrig: use stdin directly and create Volume struct
    it's a wrapper around AtomicU32, testing reading it from one thread and writing from another. lemme see if I can incorporate this into `play`.

--- src/bin/testrig.rs
+++ src/bin/testrig.rs
@@ -1,63 +1,128 @@
-use std::thread;
+use std::{
+    io::Write,
+    str::FromStr,
+    sync::{atomic::Ordering, Arc},
+    thread,
+    time::Duration,
+};
 
-use crossbeam::channel;
-use rustyline::{error::ReadlineError, DefaultEditor};
-use tracing_unwrap::*;
+use clap::Parser;
+use std::sync::atomic::AtomicU8;
+use tracing_unwrap::ResultExt;
 use wigglyair::configuration;
+
+#[derive(Debug)]
+enum VolumeError {
+    InvalidValue(u8),
+    InvalidString(String),
+}
+
+#[derive(Debug)]
+struct Volume(AtomicU8);
+
+impl Volume {
+    const MAX: u8 = 100;
+
+    fn unsafe_from(initial: u8) -> Self {
+        Self(AtomicU8::new(initial))
+    }
+
+    fn get(&self) -> u8 {
+        self.0.load(Ordering::Acquire)
+    }
+
+    fn set(&self, value: u8) -> Result<(), VolumeError> {
+        if value > Self::MAX {
+            Err(VolumeError::InvalidValue(value))
+        } else {
+            self.0.store(value, Ordering::Release);
+            Ok(())
+        }
+    }
+
+    fn set_from_string(&self, value: String) -> Result<(), VolumeError> {
+        let value: u8 = value
+            .trim()
+            .parse()
+            .map_err(|_| VolumeError::InvalidString(value))?;
+        self.set(value)
+    }
+}
+
+impl Default for Volume {
+    fn default() -> Self {
+        Self::unsafe_from(Self::MAX)
+    }
+}
+
+impl TryFrom<u8> for Volume {
+    type Error = VolumeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        if value > Self::MAX {
+            Err(VolumeError::InvalidValue(value))
+        } else {
+            Ok(Self::unsafe_from(value))
+        }
+    }
+}
+
+impl TryFrom<String> for Volume {
+    type Error = VolumeError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let value: u8 = value
+            .trim()
+            .parse()
+            .map_err(|_| VolumeError::InvalidString(value))?;
+        Self::try_from(value)
+    }
+}
+
+impl FromStr for Volume {
+    type Err = VolumeError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        Self::try_from(value.to_string())
+    }
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[clap(short, long, default_value = "100")]
+    volume: u8,
+}
 
 #[tokio::main]
 async fn main() {
     let _guard = configuration::setup_tracing_async("testrig".into());
 
-    tracing::info!("doing it");
-    let mut rl = DefaultEditor::new().unwrap_or_log();
+    let cli = Cli::parse();
+    let volume = Volume::try_from(cli.volume).expect_or_log("Could not parse volume");
+    let volume = Arc::new(volume);
 
-    if rl.load_history("history.txt").is_err() {
-        tracing::debug!("no previous history");
-    }
+    let vol1 = volume.clone();
+    let h1 = thread::spawn(move || loop {
+        write!(std::io::stderr(), "> ").unwrap();
 
-    // create a channel to send a string
-    let (tx, rx) = channel::unbounded::<String>();
-
-    let h1 = thread::spawn(move || {
-        loop {
-            let readline = rl.readline(">> ");
-            match readline {
-                Ok(line) => {
-                    if let Err(error) = rl.add_history_entry(line.as_str()) {
-                        tracing::debug!(%error, "failed to add history entry");
-                    };
-
-                    if let Err(error) = tx.send(line) {
-                        tracing::debug!(%error, "failed to send message");
-                    };
-                }
-                Err(ReadlineError::Interrupted) => {
-                    println!("CTRL-C");
-                    break;
-                }
-                Err(ReadlineError::Eof) => {
-                    println!("CTRL-D");
-                    break;
-                }
-                Err(err) => {
-                    println!("Error: {:?}", err);
-                    break;
-                }
-            }
+        let mut msg = String::new();
+        if let Err(error) = std::io::stdin().read_line(&mut msg) {
+            tracing::error!("error reading line: {}", error);
+            break;
         }
-        if let Err(error) = rl.save_history("history.txt") {
-            tracing::debug!(%error, "failed to save history");
-        };
+        if msg == "" {
+            break;
+        }
+        match vol1.set_from_string(msg) {
+            Ok(()) => tracing::debug!(volume = vol1.get(), "set volume"),
+            Err(error) => tracing::error!("error setting volume: {:?}", error),
+        }
     });
 
-    let h2 = thread::spawn(move || {
-        while let Ok(msg) = rx.recv() {
-            if msg == "" {
-                break;
-            }
-            println!("got message: {} (len={})", msg, msg.len());
-        }
+    let h2 = thread::spawn(move || loop {
+        let vol = volume.get();
+        tracing::debug!(volume = vol, "reading volume");
+        thread::sleep(Duration::from_secs(1));
     });
 
     h1.join().unwrap();
