9c934ef3b629d42b3db2f3480bc0141f5f2d8885
Brian J Brennan <brianloveswords@gmail.com>
2023-08-17 22:50:31 -0400

    testrig: experimenting with ratatui

--- src/bin/testrig.rs
+++ src/bin/testrig.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("ok");
+use std::{
+    error::Error,
+    io::{self, Stdout},
+    time::Duration,
+};
+
+use anyhow::Result;
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{prelude::*, widgets::*};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut terminal = setup_terminal()?;
+    run(&mut terminal)?;
+    restore_terminal(&mut terminal)?;
+    Ok(())
+}
+
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
+    let mut stdout = io::stdout();
+    enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen)?;
+    Ok(Terminal::new(CrosstermBackend::new(stdout))?)
+}
+
+fn restore_terminal(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+) -> Result<(), Box<dyn Error>> {
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen,)?;
+    Ok(terminal.show_cursor()?)
+}
+
+fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
+    let mut volume = 0i32;
+    Ok(loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new(format!("volume={}", volume));
+            frame.render_widget(greeting, frame.size());
+        })?;
+        if event::poll(Duration::from_millis(250))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Char('c') if is_holding_ctrl(key) => break,
+                    KeyCode::Up => {
+                        let n = volume_modifier(key);
+                        volume = if volume + n < 100 { volume + n } else { 100 }
+                    }
+                    KeyCode::Down => {
+                        let n = volume_modifier(key);
+                        volume = if volume - n > 0 { volume - n } else { 0 };
+                    }
+                    _ => {}
+                }
+            }
+        }
+    })
+}
+
+fn volume_modifier(key: KeyEvent) -> i32 {
+    if is_holding_shift(key) {
+        10
+    } else {
+        1
+    }
+}
+
+fn is_holding_shift(key: KeyEvent) -> bool {
+    key.modifiers.contains(event::KeyModifiers::SHIFT)
+}
+
+fn is_holding_ctrl(key: KeyEvent) -> bool {
+    key.modifiers.contains(event::KeyModifiers::CONTROL)
 }
