b6757f5127cd6969977f673bd96daadd7df29517
Brian J Brennan <brianloveswords@gmail.com>
2023-08-19 00:04:40 -0400

    [wip] getting close, but sample tracking doesn't work yet
    this is very close. instead of trying to fix this, I think I want to start porting over stuff from `play` and try to get it making sound.
    
    before that, I might add support for pause state.

--- src/bin/testrig.rs
+++ src/bin/testrig.rs
@@ -1,7 +1,13 @@
 use std::{
     error::Error,
     io::{self, Stdout},
-    path::Path,
+    path::{Path, PathBuf},
+    rc::Rc,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    thread::{self, JoinHandle},
     time::Duration,
 };
 
@@ -12,9 +18,10 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use itertools::FoldWhile::*;
 use itertools::Itertools;
+use metaflac::Tag;
 use ratatui::{prelude::*, widgets::*};
-// use rusqlite::{Connection, Result as SqlResult};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -25,17 +32,28 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-    let files = cli
+    let tracks: TrackList = cli
         .files
         .iter()
         .map(|f| Path::new(f))
         .filter(|p| p.exists() && p.extension().unwrap_or_default() == "flac")
-        .map(|p| p.file_name().unwrap().to_string_lossy().to_string())
-        .collect::<Vec<_>>();
-    assert!(!files.is_empty(), "No files to play");
+        .map(|p| (p, Tag::read_from_path(p).unwrap()))
+        .map(|(p, tag)| {
+            let path = p.to_owned();
+            let samples = tag
+                .get_streaminfo()
+                .map(|si| si.total_samples)
+                .unwrap_or_default();
+            Track { path, samples }
+        })
+        .collect_vec()
+        .into();
+
+    let current_sample = Arc::new(AtomicU64::new(0));
 
     let mut terminal = setup_terminal()?;
-    run(&mut terminal, files)?;
+    start_sample_ticker(Arc::clone(&current_sample), tracks.total_samples);
+    run(&mut terminal, tracks, Arc::clone(&current_sample))?;
     restore_terminal(&mut terminal)?;
     Ok(())
 }
@@ -55,12 +73,32 @@ fn restore_terminal(
     Ok(terminal.show_cursor()?)
 }
 
+fn start_sample_ticker(current_sample: Arc<AtomicU64>, total_samples: u64) -> JoinHandle<()> {
+    thread::spawn(move || {
+        current_sample.store(0, Ordering::SeqCst);
+        loop {
+            thread::sleep(Duration::from_millis(100));
+            let delta = 9600;
+            let previous = current_sample.fetch_add(delta, Ordering::SeqCst);
+            if previous + delta >= total_samples {
+                break;
+            }
+        }
+    })
+}
+
 fn run(
     terminal: &mut Terminal<CrosstermBackend<Stdout>>,
-    files: Vec<String>,
+    tracks: TrackList,
+    current_sample: Arc<AtomicU64>,
 ) -> Result<(), Box<dyn Error>> {
+    let tracks = Rc::new(tracks);
+    let total_samples = tracks.total_samples;
     let mut volume = 100i16;
     Ok(loop {
+        let current_sample = current_sample.load(Ordering::SeqCst);
+        let ratio = current_sample as f64 / total_samples as f64;
+        let current_track = tracks.find_playing(current_sample);
         terminal.draw(|f| {
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
@@ -80,15 +118,17 @@ fn run(
                 .percent(volume as u16);
             f.render_widget(gauge, chunks[0]);
 
-            let items = files
+            let items = tracks
+                .tracks
                 .iter()
-                .map(|f| ListItem::new(f.as_str()))
-                .enumerate()
-                .map(|(i, mut li)| {
-                    if i == 3 {
-                        li = li.style(Style::default().fg(Color::Green).bold());
-                    }
-                    li
+                .map(|t| {
+                    let style = Style::default();
+                    let style = if t == current_track {
+                        style.fg(Color::Green).bold()
+                    } else {
+                        style.fg(Color::White)
+                    };
+                    ListItem::new(t.path.file_name().unwrap().to_string_lossy()).style(style)
                 })
                 .collect_vec();
             let list = List::new(items)
@@ -98,7 +138,8 @@ fn run(
 
             let gauge = Gauge::default()
                 .gauge_style(Style::default().fg(Color::Yellow).bg(Color::Black))
-                .ratio(0.35);
+                .ratio(ratio)
+                .label(format!("{} / {}", current_sample, total_samples));
             f.render_widget(gauge, chunks[2]);
         })?;
 
@@ -136,4 +177,66 @@ fn is_holding_shift(key: KeyEvent) -> bool {
 
 fn is_holding_ctrl(key: KeyEvent) -> bool {
     key.modifiers.contains(event::KeyModifiers::CONTROL)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Track {
+    path: PathBuf,
+    samples: u64,
+}
+
+struct TrackList {
+    tracks: Vec<Track>,
+    total_samples: u64,
+}
+
+#[allow(dead_code)]
+impl TrackList {
+    fn new() -> Self {
+        Self {
+            tracks: Vec::new(),
+            total_samples: 0,
+        }
+    }
+
+    fn add_track(&mut self, track: Track) {
+        self.total_samples += track.samples;
+        self.tracks.push(track);
+    }
+
+    fn add_tracks(&mut self, tracks: Vec<Track>) {
+        self.total_samples += tracks.iter().map(|t| t.samples).sum::<u64>();
+        self.tracks.extend(tracks);
+    }
+
+    fn find_playing(&self, current_sample: u64) -> &Track {
+        let (found, _) = self
+            .tracks
+            .iter()
+            .enumerate()
+            .fold_while((0usize, 0u64), |(i, mut total), (j, track)| {
+                total += track.samples;
+                if total > current_sample {
+                    Done((i, total))
+                } else {
+                    Continue((j, total))
+                }
+            })
+            .into_inner();
+        &self.tracks[found]
+    }
+}
+
+impl Default for TrackList {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Into<TrackList> for Vec<Track> {
+    fn into(self) -> TrackList {
+        let mut tl = TrackList::new();
+        tl.add_tracks(self);
+        tl
+    }
 }
